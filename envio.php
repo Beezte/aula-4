7e3ee8729ca839ff45c9b262d901f2d728e7fcc4
Kawã <kawabazante@gmail.com>
2023-07-18 15:52:19 -0300

    backend matricula

---
+++ envio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>IFPE - HISTÓRIA DO CAMPUS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div class="cabecalho">
+        <img src="arquivos/logo-reitoria.png" alt="LOGOIFPE">
+    </div>
+    <div class="menu-principal">
+        <a href="index.html">Página Inicial</a> &nbsp; | &nbsp; <a href="historia.html">História do Campus</a> &nbsp; | &nbsp; <a href="imagens.html">Imagens</a> &nbsp; | &nbsp; <a href="noticias.html">Notícias</a> &nbsp; | &nbsp; <a href="matricula.html">Matricule-se</a>
+    </div>
+    <hr class="menu-hr">
+</header>
+
+<?php
+$conexao = mysqli_connect("localhost", "root", "root", "bazante");
+if(!$conexao){
+    echo"deu erro";
+}
+echo"conectou"
+?>
+
+</body>
+</html>
